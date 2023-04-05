473fdcd7bf3df7bfe70175c2828135719ec38be2
bhunao <bhunao@gmail.com>
2023-04-04 22:34:49 -0300

    testing for performance, i guess

--- base_project.py
+++ base_project.py
@@ -40,6 +40,15 @@ system = System()
 player = Entity(groups=system)
 player.move(5, 5)
 
+n_times = 1_000_000
+player.rect.center = 0, 0
+print('System::move', timeit.timeit(lambda: System.move(player, 1, 1), number=n_times))
+player.rect.center = 0, 0
+print('player::move', timeit.timeit(lambda: player.move(1, 1), number=n_times))
+player.rect.center = 0, 0
+print('func::move  ', timeit.timeit(lambda: player.move(1, 1), number=n_times))
+player.rect.center = 0, 0
+print('+'*50)
     
 while 1:
     # background
