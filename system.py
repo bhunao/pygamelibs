2f8a86fc0f9e3309a2782c37b739820410bc24e5
bhunao <bhunao@gmail.com>
2023-04-05 00:33:20 -0300

    Sequence instead of List

--- system.py
+++ system.py
@@ -4,7 +4,7 @@ import pygame
 import config
 
 from abc import ABC
-from typing import List
+from typing import List, Sequence
 
 from pygame import display, Surface, init
 from pygame.time import Clock
@@ -12,7 +12,7 @@ from pygame.transform import scale
 
 
 class Entity(ABC):
-    pos: List[int] = [0, 0]
+    pos: Sequence[int] = [0, 0]
 
 class System(ABC):
     _entities: List[Entity] = []
@@ -27,7 +27,7 @@ class System(ABC):
         return True
 
     @staticmethod
-    def move(entity: Entity, pos: list[int]) -> bool:
+    def move(entity: Entity, pos: Sequence[int]) -> bool:
         entity.pos[0] += pos[0]
         entity.pos[1] += pos[1]
         return True
@@ -65,4 +65,5 @@ def main():
         pygame.display.flip()
         clock.tick(15)
 
-main()
+if __name__ == '__main__':
+    main()
